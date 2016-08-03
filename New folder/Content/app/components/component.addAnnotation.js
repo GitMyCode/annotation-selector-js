3e96c1227e898ed79a5d47c4906b6def251245ff
martin bouchard <marbouchl@gmail.com>
2016-08-02 22:55:26 -0400

    avec electron js

---
+++ New folder/Content/app/components/component.addAnnotation.js
@@ -0,0 +1,39 @@
+(function () {
+    merlin.AddAnnotationComponent = function (annotations, selectionData, callback) {
+        this.selectionData = selectionData;
+        this.annotations = annotations;
+        this.callback = callback;
+        this.classificationData = {
+            comment: ko.observable(),
+            theme: ko.observable()
+        }
+
+        this.themeOptions = [
+            {
+                value: "Satisfaction",
+                text: "Satisfaction"
+            }, {
+                value: "RelationShip with manager",
+                text: "RelationShip with manager"
+            }];
+    }
+
+    merlin.AddAnnotationComponent.prototype = {
+        save: function () {
+            this.annotations.push({
+                selectionData: this.selectionData,
+                classificationData: this.classificationData
+            });
+
+            this._close();
+        },
+
+        cancel: function () {
+            this._close();
+        },
+
+        _close: function () {
+            this.callback();
+        }
+    }
+})();
